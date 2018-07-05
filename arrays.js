5a7f4ac0a19eb9f93b79b6ee0ca5d88f0d6ebe67
Jonathan Nguyen <jnnj12@yahoo.com>
2018-07-05 21:03:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,28 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element) {
+<<<<<<< HEAD
   array = [element,...array]
   return array
+=======
+  console.log(array = [element,...array])
+>>>>>>> 4df280919e291d6a46fe186504ded8482d5197c7
 }
 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
 
+<<<<<<< HEAD
   array.unshift(element)
   return array
+=======
+  console.log(array.unshift(element))
+>>>>>>> 4df280919e291d6a46fe186504ded8482d5197c7
 }
 
 
 function addElementToEndOfArray(array, element){
+<<<<<<< HEAD
   array = [...array, element]
   return array
 }
@@ -47,4 +56,11 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 function removeElementFromEndOfArray(array){
   return array.slice(0, array.length-1)
+=======
+  console.log (array.push(element))
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  console.log (array.push(element))
+>>>>>>> 4df280919e291d6a46fe186504ded8482d5197c7
 }
